75456a6618de54308922201ad3fffd19d071b57e
Dipali-Ahirrao <128622851+Dipali-Ahirrao@users.noreply.github.com>
2023-06-15 15:26:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+function App(){
+
+  return(
+    <>
+      <Message/>
+      <Message/>
+      <Message/>
+    </>
+    
+  )
+}
+
+function Message(){
+  return(
+    <h1>Hello World</h1>
+  )
+}
+
+export default App;
