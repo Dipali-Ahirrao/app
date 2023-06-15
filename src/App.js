e55333f1f93a8e3489e73055fa79d1d3a0567806
Dipali-Ahirrao <128622851+Dipali-Ahirrao@users.noreply.github.com>
2023-06-15 16:33:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ function App(){
 
   return(
     <>
-      <Message/>
-      <Message/>
-      <Message/>
+      <Message name="Dipali"/>
+      <Message name ="Yamini" age = "27"/>
+      <Message id="1" name="Rani" age = "27"/>
     </>
     
   )
 }
 
-function Message(){
+function Message({age ,name}){
+ 
   return(
-    <h1>Hello World</h1>
+    <>
+
+     { /**<h1>Hello {para} </h1> */} 
+
+      <h1>Hello {name}</h1>
+      <h1>Hello {age}</h1>
+      <h1>Hello {name} {age}</h1>
+      
+    </>
+    
   )
 }
 
