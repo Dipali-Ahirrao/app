e98445ca23940c13ecccd5962ea66fbcacefd165
Dipali-Ahirrao <128622851+Dipali-Ahirrao@users.noreply.github.com>
2023-06-15 17:10:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,66 @@ function App(){
 
   return(
     <>
-      <Message/>
-      <Message/>
-      <Message/>
+      <Message />
     </>
     
   )
 }
 
 function Message(){
+  let name = "Dipali";
+  let list = [ "Delhi", "Nashik", "Mumbai"];
+
+ 
   return(
-    <h1>Hello World</h1>
+    <>
+      <h1>static data (variable) : {name}</h1>
+
+      {/** consumer function */}
+
+      {   list.forEach((item) => 
+        item )
+      } 
+      
+      {/** map method is transformer it take input and return output 
+       * 
+       * output comes in one line there is lack of readiability
+      */}
+
+      {
+        list.map((item) => item)
+      }
+
+      <hr/>
+      <h1>static string  and list element</h1>
+      {
+        list.map((item)=>`Hello ${item} `)
+      }
+
+      <hr/>
+      <h1>print static string using list iteration  </h1>
+
+      {
+        list.map((item)=>"Hello World ")
+      }
+
+      <hr/>
+      <h1>print static string using list iteration  </h1>
+
+      {
+        list.map((item) =>
+        (
+          <div>
+            <span> Hello {item}</span>
+          </div>
+
+        ))
+      }
+
+      
+
+    </>
+    
   )
 }
 
